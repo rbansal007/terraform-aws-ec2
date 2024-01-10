6b2781c9a62f7c7334a489d582afdb60c5b42048
Ramit Bansal <ramit.bansal@hashicorp.com>
2024-01-10 10:24:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
+terraform {
+  cloud {
+    hostname = "ec2-13-201-48-181.ap-south-1.compute.amazonaws.com"
+    organization = "test21stDeC"
+    workspaces {
+      name = "8thJan"
+    }
+  }
+ required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
 
+provider "random" {
+  # Configuration options
+}
+
+
+resource "random_string" "random" {
+  length           = 16
+  special          = true
+  override_special = "/@£$"
+}
